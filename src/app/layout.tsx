2e4ea4eff0805e8bef406d33b51183da1fe977de
Trần Đức Toàn <toan.td.uet@gmail.com>
2024-06-17 08:51:00 +0000

    Add analysis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat as FontSans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 
 import { ThemeProvider } from "@/components/theme-provider";
 
@@ -30,6 +31,7 @@ export default function RootLayout({
             {children}
           </main>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
