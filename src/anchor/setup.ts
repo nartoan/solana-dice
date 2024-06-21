91dd20a5250ea4c95581006d711f4ff9fff32bf7
Dennis <dck28@users.noreply.github.com>
2024-06-22 00:11:20 +0800

    Fix place bet using Anchor Wallet

--- src/anchor/setup.ts
+++ src/anchor/setup.ts
@@ -3,26 +3,23 @@
 import { AnchorProvider, Idl, Program } from "@coral-xyz/anchor";
 import IDL from "./idl.json";
 import {
-  clusterApiUrl,
-  Commitment,
-  Connection,
   PublicKey,
 } from "@solana/web3.js";
-import { SOLANA_NETWORK } from "@/const";
-// import { useConnection } from "@solana/wallet-adapter-react";
+import { AnchorWallet, useConnection, useAnchorWallet } from "@solana/wallet-adapter-react";
 
 const programID = new PublicKey("B4xgkC1id6Liyk6TvojjtP4BvRPPipTr8iriLFy8ywTb");
 const housePublicKey = new PublicKey(
   "4fiLLb9Lxaa9iB8gGFPJvy81NRDBCA15q4Jw2ZekQ1U9"
 );
 
-const opts = {
-  preflightCommitment: "processed" as Commitment,
-};
-
 export const useAnchor = () => {
-  const connection = new Connection(clusterApiUrl(SOLANA_NETWORK) , opts.preflightCommitment);
-  const provider = new AnchorProvider(connection, window.solana, opts);
+  const { connection } = useConnection();
+  const wallet = useAnchorWallet();
+  const provider = new AnchorProvider(
+    connection,
+    wallet as unknown as AnchorWallet,
+    {}
+  );
   // Initialize the program interface with the IDL, program ID, and connection.
   // This setup allows us to interact with the on-chain program using the defined interface.
   const program = new Program(IDL as Idl, programID, provider);
