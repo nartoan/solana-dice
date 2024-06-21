312dcf9366786b96009c20f71f8e474f214e2fa4
nartoan <toan.td.uet@gmail.com>
2024-06-22 01:08:15 +0700

    fix: correct render wallet modal provider

--- src/provider/solana.tsx
+++ src/provider/solana.tsx
@@ -5,20 +5,28 @@ import {
   ConnectionProvider,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 
 import "@solana/wallet-adapter-react-ui/styles.css";
 import { SOLANA_NETWORK } from "@/const";
+import dynamic from "next/dynamic";
+
+const ReactUIWalletModalProviderDynamic = dynamic(
+  async () =>
+    (await import("@solana/wallet-adapter-react-ui")).WalletModalProvider,
+  { ssr: false }
+);
 
 export const SolanaProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  const endpoint = useMemo(() => clusterApiUrl(SOLANA_NETWORK), [SOLANA_NETWORK]);
+  const endpoint = useMemo(() => clusterApiUrl(SOLANA_NETWORK), []);
 
   const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
 
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets}>
-        <WalletModalProvider>{children}</WalletModalProvider>
+        <ReactUIWalletModalProviderDynamic>
+          {children}
+        </ReactUIWalletModalProviderDynamic>
       </WalletProvider>
     </ConnectionProvider>
   );
