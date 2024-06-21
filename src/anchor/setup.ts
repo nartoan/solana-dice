1d88205879a39694a97f81aa76d2ebf2c1a9d3a7
Dennis <dck28@users.noreply.github.com>
2024-06-21 23:35:33 +0800

    Attempt to use AnchorProvider

--- src/anchor/setup.ts
+++ src/anchor/setup.ts
@@ -1,23 +1,31 @@
 "use client";
 
-import { Idl, Program } from "@coral-xyz/anchor";
+import { AnchorProvider, Idl, Program } from "@coral-xyz/anchor";
 import IDL from "./idl.json";
 import {
+  clusterApiUrl,
+  Commitment,
+  Connection,
   PublicKey,
 } from "@solana/web3.js";
-import { useConnection } from "@solana/wallet-adapter-react";
+import { SOLANA_NETWORK } from "@/const";
+// import { useConnection } from "@solana/wallet-adapter-react";
 
 const programID = new PublicKey("B4xgkC1id6Liyk6TvojjtP4BvRPPipTr8iriLFy8ywTb");
 const housePublicKey = new PublicKey(
   "4fiLLb9Lxaa9iB8gGFPJvy81NRDBCA15q4Jw2ZekQ1U9"
 );
 
-export const useAnchor = () => {
-  const { connection } = useConnection();
+const opts = {
+  preflightCommitment: 'processed',
+};
 
+export const useAnchor = () => {
+  const connection = new Connection(clusterApiUrl(SOLANA_NETWORK) , opts.preflightCommitment as Commitment);
+  const provider = new AnchorProvider(connection, window.solana, opts);
   // Initialize the program interface with the IDL, program ID, and connection.
   // This setup allows us to interact with the on-chain program using the defined interface.
-  const program = new Program(IDL as Idl, programID, { connection });
+  const program = new Program(IDL as Idl, programID, provider);
 
   const [payoutHistoryPda] = PublicKey.findProgramAddressSync(
     [Buffer.from("payout_history"), housePublicKey.toBuffer()],
