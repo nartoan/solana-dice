8c6feccfed2ac2cf9c41da365ccd69b37c2e194a
nartoan <toan.td.uet@gmail.com>
2024-07-10 04:49:26 +0700

    responsive

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -9,20 +9,21 @@ import xSvg from "@/assets/img/x.svg";
 import telegramSvg from "@/assets/img/telegram.svg";
 import { Button } from "@/components/ui/button";
 
-
 export default function Home() {
   return (
     <div className="w-full max-w-xl">
       <Header />
-      <div className="flex flex-col items-center mt-20">
+      <div className="flex flex-col items-center mt-20 md:mt-40">
         <Image src={diceSvg} alt={"Dice icon"} />
         <LabelCustom classNameContainer="mt-4 text-[32px]">
           大 Big & Small 細
         </LabelCustom>
-        <Button className="rounded-none mt-10 w-4/5">ENTER GAME</Button>
-        <Button className="rounded-none mt-2 w-4/5" variant="outline">
-          ROAD MAP
-        </Button>
+        <div className="flex flex-col md:flex-row gap-4 mt-10 w-4/5">
+          <Button className="rounded-none w-full">ENTER GAME</Button>
+          <Button className="rounded-none w-full" variant="outline">
+            ROAD MAP
+          </Button>
+        </div>
         <div className="flex gap-6 mt-24">
           <Button size="icon">
             <Image src={xSvg} alt={"X"} />
