d377b9ce8b1f71c0c006973440fc660d5d7b4494
nartoan <toan.td.uet@gmail.com>
2024-07-12 23:20:04 +0700

    fix: image on fb

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,17 +9,19 @@ import "./globals.css";
 const fontSans = FontSans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://degen-dice.vercel.app/'),
+  metadataBase: new URL("https://degen-dice.vercel.app/"),
   title: "Degen Dice",
   description: "Welcome to a game of Big & Small Dice",
   openGraph: {
     title: "Degen Dice",
     description: "Welcome to a game of Big & Small Dice",
-    images: [{
-      url: "../og-image.png",
-      width: 200,
-      height: 200
-    }],
+    images: [
+      {
+        url: "./opengraph-image.png",
+        width: 200,
+        height: 200,
+      },
+    ],
   },
 };
 
