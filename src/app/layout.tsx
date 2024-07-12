34a6f5ec57eeb18fa6592f7caf9561b39a6f67b2
nartoan <toan.td.uet@gmail.com>
2024-07-12 08:57:33 +0700

    fix: add min-width

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="flex min-h-screen flex-col items-center justify-between p-4">
+          <main className="flex min-h-screen flex-col items-center justify-between p-4 min-w-[330px]">
             {children}
           </main>
         </ThemeProvider>
