0db726acd6bfd608737a8dfc9ca2be99c962ccc2
Trần Đức Toàn <toan.td.uet@gmail.com>
2024-07-12 03:13:12 +0000

    Update color footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const fontSans = localFont({
 
 export default function Footer({ className }: { className: string }) {
   return (
-    <div className={cn(fontSans.className, "w-full flex items-center justify-center absolute bottom-0 left-0 right-0", className)}>
+    <div className={cn(fontSans.className, "w-full flex items-center justify-center text-[hsla(0,0%,100%,.25)]", "absolute bottom-0 left-0 right-0", className)}>
       COPYRIGHT (C) 2024, DEGEN DICE
     </div>
   );
